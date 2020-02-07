b0aba4383e812826243ffd230317e505d934a338
Helena Huang <yuhui.huang77@gmail.com>
2020-02-07 11:20:12 -0800

    Creating file for an image

--- Preprocessing/patch.py
+++ Preprocessing/patch.py
@@ -1,2 +1,42 @@
 # Write images into patches
+import matplotlib.image as mpimg 
+import matplotlib.pyplot as plt 
+import numpy as np
+import json
+import os
+import constant as const
+
+def patch_write(img_name):
+	img = mpimg.imread(img_name)
+
+	# create directory
+	path = os.getcwd() + '\\' + 'test'
+	try:
+		os.mkdir(path)
+	except OSError:
+		print ("Creation of the directory %s failed" % path)
+
+	# Writing image info
+	data = {}
+	data[const.NAME] = img_name
+	with open(path + '\\' + 'data.txt', 'w') as outfile:
+		json.dump(data, outfile)
+
+	print(img.shape)
+
+
+def main():
+	# Read Images 
+	img = mpimg.imread('abigail-keenan-27293.png') 
+
+	# Write to patches
+	patch_write('abigail-keenan-27293.png')
+
+	# Output Images 
+	plt.imshow(img)
+	# plt.show()
+
+
+if __name__ == "__main__":
+    main()
 
